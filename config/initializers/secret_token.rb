b62f9c404d0290668f2ca09d78c014240f73e41c
Alexander V. Khokhlov <point@dontpanic.com.ua>
2012-01-24 14:56:39 +0100

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+YesDontpanicComUa::Application.config.secret_token = '011e19aedafd12cc3c516b7b87784b6c2483ba40505067b7c455d6cade6e3f10e6118bb1245b4571b1c6d50714b82ebadf993dd99fd105266c9be399df6616ed'
