0bac407e74e35eb6eb0653b256471600c399277f
Alexey Kovtunets <climbonn@gmail.com>
2012-02-27 01:05:39 +0200

    Added markup for all pages. Defined pages in controller. TODO: change pagess for layouts, add unique id.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,18 @@
 class PagesController < ApplicationController
+  def index
+  end
   def about
+  end
+  def blog
+  end
+  def blog_inside
+  end
+  def contact_us
+  end
+  def inside_project
+  end
+  def people
+  end
+  def profile
   end
 end
