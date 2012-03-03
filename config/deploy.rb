20595b755b087872409f4955047538f28b60ae8a
point <alex.softx@gmail.com>
2012-03-03 15:46:29 +0200

    Moving sock and pid to /tmp and /var dirs. Database.yml symlnking.

--- config/deploy.rb
+++ config/deploy.rb
@@ -48,3 +48,11 @@ role :worker, domain
 #     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
 #   end
 # end
+after 'deploy:update_code', 'deploy:symlink_db'
+
+namespace :deploy do
+  desc "Symlinks the database.yml"
+  task :symlink_db, :roles => :app do
+    run "ln -nfs #{deploy_to}/shared/config/database.yml #{release_path}/config/database.yml"
+  end
+end
