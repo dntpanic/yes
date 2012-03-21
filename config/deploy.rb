b8dcda72afe2378c2f7d2789bf082c346206e745
point <alex.softx@gmail.com>
2012-03-21 18:27:28 +0200

    Fix: deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -54,6 +54,6 @@ namespace :deploy do
   desc "Symlinks the database.yml"
   task :symlink_db, :roles => :app do
     run "ln -nfs #{deploy_to}/shared/config/database.yml #{release_path}/config/database.yml"
-    run "ln -nfs #{deploy_to}/shared/config/production.rb #{release_path}/config/production.rb"
+    run "ln -nfs #{deploy_to}/shared/config/production.rb #{release_path}/config/environments/production.rb"
   end
 end
