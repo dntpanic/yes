f6e2babaf189d4d81772f326ed6270dd92baa6ff
Alexey Mirniy <alexey.mirniy@gmail.com>
2012-05-20 18:09:02 +0300

    Content model was styled, wysiwyg was added.

--- app/admin/content.rb
+++ app/admin/content.rb
@@ -1,3 +1,30 @@
 ActiveAdmin.register Content do
-  actions :all, :except => [:show]
+  # standard 
+  #actions :all, :except => [:show]
+  
+  #Custom actions to avoid headache
+  actions :index, :new, :create, :update, :edit
+
+  filter :identifier
+  filter :content
+
+  index do
+    column :identifier
+    column :content
+    default_actions :name=> "Actions"
+  end
+
+  form  do |f|
+    f.inputs "Content block details" do
+      if !f.object.new_record?
+        f.input :identifier, :input_html => { :disabled => true },
+          :hint => 'In order to keep in safe whole site you can\'t modify this field.'
+      else
+        f.input :identifier
+      end
+      f.input :content, :hint => f.template.wysiwyg(:content_content)
+    end
+    f.buttons
+  end
+
 end
--- app/models/content.rb
+++ app/models/content.rb
@@ -1,4 +1,5 @@
 class Content < ActiveRecord::Base
+  validates_presence_of :identifier
   def self.get_content(identifier)
     return Content.where('contents.identifier = ?', identifier).first.content
   end
