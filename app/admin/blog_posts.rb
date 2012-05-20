d7c24133aae66f90b84c2b1146c05c1cf5093dd8
Alexey Mirniy <alexey.mirniy@gmail.com>
2012-05-20 17:29:04 +0300

    Blog Post: WYSIWYG and validation added.

--- app/admin/blog_posts.rb
+++ app/admin/blog_posts.rb
@@ -1,3 +1,37 @@
 ActiveAdmin.register BlogPost do
+  actions :all, :except => [:show]
+
+  filter :title
+  filter :cut
+  filter :content
+
+  index do
+    column "Title", :title
+    column "Cut" do |post|
+      strip_tags(post.cut).truncate 200
+    end
+
+    column "Content" do |post|
+      strip_tags(post.content).truncate 500
+    end
+
+    column "Updated" do |post| 
+      post.updated_at.strftime('%d %b %Y, %H:%M')
+    end
+    default_actions :name=> "Actions"
+  end
+
+  form do |f|
+    f.inputs "Blog Post Details" do
+      f.input :title, :required => true
+
+      f.input :cut, :hint => f.template.wysiwyg(:blog_post_cut),
+        :required => true
+
+      f.input :content, :hint => f.template.wysiwyg(:blog_post_content),
+        :required =>true
+    end
+    f.buttons
+  end
   
 end
--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -1,4 +1,5 @@
 class BlogPost < ActiveRecord::Base
+  validates_presence_of :title, :cut, :content
   def self.prev(id)
     return BlogPost.where('blog_posts.id < ?', id).last
   end
